0fc7b0f804be66a47429184a7345fbb969644f1d
Pahan Sarathchandra <pahan123@gmail.com>
2024-05-16 14:34:12 +0300

    Add basic layouts

---
+++ app/[slug]/edit/page.tsx
@@ -0,0 +1,7 @@
+export default function BlogPostEdit() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>blog post edit</div>
+    </main>
+  );
+}
