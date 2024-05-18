a2cdee2bea89fec1adc4871c7381ca32030e6187
Pahan Sarathchandra <pahan123@gmail.com>
2024-05-18 14:03:47 +0300

    Connect database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
-export default function HomePage() {
+import { fetchAllPosts } from "@/app/lib/data";
+import { PostsList } from "./components/PostsList";
+export default async function HomePage() {
+  const blogPosts = await fetchAllPosts();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello World
+    <main className="">
+      <PostsList posts={blogPosts}></PostsList>
     </main>
   );
 }
