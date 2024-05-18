a81d5f3a126e3bb5d0045b5d9034ab869d6a0f30
Pahan Sarathchandra <pahan123@gmail.com>
2024-05-18 14:35:26 +0300

    Fix build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import { fetchAllPosts } from "@/app/lib/data";
 import { PostsList } from "./components/PostsList";
+import { Suspense } from "react";
 export default async function HomePage() {
   const blogPosts = await fetchAllPosts();
   return (
     <main className="">
-      <PostsList posts={blogPosts}></PostsList>
+      <Suspense fallback={<p>Loading...</p>}>
+        <PostsList posts={blogPosts}></PostsList>
+      </Suspense>
     </main>
   );
 }
