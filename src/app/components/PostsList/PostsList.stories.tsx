837d33d4b7a5cb27b93fc421ae49237ac20746f5
Pahan Sarathchandra <pahan123@gmail.com>
2024-05-19 16:32:27 +0300

    Fix build

--- src/app/components/PostsList/PostsList.stories.tsx
+++ src/app/components/PostsList/PostsList.stories.tsx
@@ -4,6 +4,7 @@ import { BlogPost } from "./PostsList";
 
 const posts: Array<BlogPost> = Array.from({ length: 10 }, (_, i) => {
   return {
+    id: i,
     title: `hello world${i}`,
     description: `${i}Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.`,
     author: "Pahan",
