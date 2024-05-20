bcfe73759b13a214ffa54ed952b4b76566673351
Pahan Sarathchandra <pahan123@gmail.com>
2024-05-20 20:12:45 +0300

    Update seed.ts to insert random words

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,10 +1,15 @@
-import { db, sql } from "@/lib/kysely";
-
 import dotenv from "dotenv";
-
+// this line should execute before other imports.
 dotenv.config({
   path: [".env.development.local", ".env.local", ".env"],
 });
+
+import randomWords from "better-random-words";
+import { db, sql } from "../src/lib/kysely";
+
+const POST_COUNT = 1000;
+const DESC_LENGTH = 200;
+const TITLE_LENGTH = 4;
 
 async function seedPosts() {
   try {
@@ -19,15 +24,25 @@ async function seedPosts() {
       )
       .execute();
     console.log(`Created "posts" table`);
-    await db
-      .insertInto("posts")
-      .values([
-        {
-          title: "Hello",
-          description: "Hello world",
-        },
-      ])
-      .execute();
+
+    const posts = [];
+
+    for (let i = 0; i < POST_COUNT; i++) {
+      const title = randomWords({
+        exactly: 1,
+        wordsPerString: TITLE_LENGTH,
+      })[0];
+      const description = randomWords({
+        exactly: 1,
+        wordsPerString: DESC_LENGTH,
+      })[0];
+      posts.push({
+        title: title,
+        description: description,
+      });
+    }
+
+    await db.insertInto("posts").values(posts).execute();
   } catch (error) {
     console.log(error);
   }
